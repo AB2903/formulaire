c4d5dff0402174fd91440b72f382c79201bcd074
AB2903 <baroyel3@gmail.com>
2024-09-23 16:40:50 +0000

    another

--- JS/inscription.js
+++ JS/inscription.js
@@ -1,11 +1,20 @@
 let btn = document.getElementById("btn");
 
-btn.addEventListener("click", () => {
+btn.addEventListener("click", (e) => {
+
     let prenom = document.getElementById("prenom").value;
     let nom = document.getElementById("nom").value;
     let mail = document.getElementById("mail").value;
     let pwd = document.getElementById("pwd").value;
     let pwd1 = document.getElementById("pwd1").value;
 
-    
+    if(prenom === '' || nom === '' || mail === '' || pwd === '' || pwd1 === ''){
+        alert("Il faut tout remplir !!!");
+        e.preventDefault();
+    } else if (pwd !== pwd1) {
+        alert("Les deux mots de passe ne sont pas identiques");
+    } else{
+        alert("Votre inscription est validée !!!");
+    }
+
 })
